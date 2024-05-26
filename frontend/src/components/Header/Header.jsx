981d8037bf56d1c1e59076675b89d0d7bc9406a2
Julia Kamuda <julka.kamuda@gmail.com>
2024-05-27 00:51:50 +0200

    updated styles

--- frontend/src/components/Header/Header.jsx
+++ frontend/src/components/Header/Header.jsx
@@ -26,7 +26,7 @@ const Header = () => {
         if (userRole === 'Parent') {
             navigate('/parent-rewards');
         } else if (userRole === 'Child') {
-            navigate('/rewards');
+            navigate('/myrewards');
         }
     };
 
